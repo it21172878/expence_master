1b4fd55d1c22be8101efc6ed99183c33729bdadf
it21172878 <99156192+it21172878@users.noreply.github.com>
2023-11-18 11:31:36 +0530

    List view , context and builder

--- lib/pages/expences.dart
+++ lib/pages/expences.dart
@@ -1,3 +1,4 @@
+import 'package:expence_master/models/expence.dart';
 import 'package:flutter/material.dart';
 
 class Expences extends StatefulWidget {
@@ -8,22 +9,53 @@ class Expences extends StatefulWidget {
 }
 
 class _ExpencesState extends State<Expences> {
+// expenceList
+  final List<ExpenceModel> _expenceList = [
+    ExpenceModel(
+        title: "Foot Ball",
+        amount: 12.5,
+        date: DateTime.now(),
+        category: Category.leasure),
+    ExpenceModel(
+        title: "Avacardo",
+        amount: 10.25,
+        date: DateTime.now(),
+        category: Category.food),
+    ExpenceModel(
+        title: "London",
+        amount: 1002.50,
+        date: DateTime.now(),
+        category: Category.travel)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expence Master'),
-        backgroundColor: Color.fromARGB(255, 202, 124, 245),
+        backgroundColor: const Color.fromARGB(255, 202, 124, 245),
         elevation: 0,
         actions: [
           Container(
-            color: Color.fromARGB(255, 236, 215, 248),
+            color: const Color.fromARGB(255, 236, 215, 248),
             child: IconButton(
               onPressed: () {},
               icon: const Icon(
                 Icons.add,
                 color: Colors.black,
               ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _expenceList.length,
+              itemBuilder: (context, index) {
+                return Text(_expenceList[index].title);
+              },
             ),
           ),
         ],
